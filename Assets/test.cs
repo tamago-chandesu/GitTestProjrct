f8c576baa32dc967813971d01c80a093101c15e1
YourName <medamayaki0926@gmail.com>
2020-06-12 12:14:51 +0900

    HELLO追加

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
     }
 
     // Update is called once per frame
