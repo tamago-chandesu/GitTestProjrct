5f6c6e91d4eae49011155ef832fa274107367076
noykks <ksnoyk@gmail.com>
2020-06-19 12:19:16 +0900

    加瀬　スクリプト追加

---
+++ Assets/KaseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaseScript : MonoBehaviour
+{
+
+    int a = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(a);
+    }
+}
