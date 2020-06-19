80837c40cb3851af76ad5292ae40b9814eddb022
Yamada-Tatsuaki <ed.from.ac@gmail.com>
2020-06-19 12:40:04 +0900

    tamagoEdit

--- Assets/TamagoScript.cs
+++ Assets/TamagoScript.cs
@@ -7,12 +7,12 @@ public class TamagoScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("aaaaa");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("iiiii");
     }
 }
